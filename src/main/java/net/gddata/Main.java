6e316eb7f7e7781f3d6252caffcc966d3af4f5f3
张振峰 <xyls_ok@126.com>
2017-06-09 10:53:31 +0800

    tewt

--- src/main/java/net/gddata/Main.java
+++ src/main/java/net/gddata/Main.java
@@ -1,9 +1,10 @@
 package net.gddata;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -15,6 +16,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
 
         Properties properties = new Properties();
         File file = new File("/Users/zhangzf/Codes/javaProject/AutoCreateProject/src/main/resources/info.properties");
@@ -56,8 +58,13 @@ public class Main {
                 }
 
                 String projectPath = proPath + proName;//项目路径
+
+
                 File file1 = new File(projectPath);
+
+                boolean isCreate = true;
                 if (file1.exists()) {
+                    isCreate = false;
                     Scanner sc = new Scanner(System.in);
                     System.out.println("该路径存在相同项目名称: " + proName + " 确定要删除重建吗？（y/n）");
                     String value = sc.nextLine();
@@ -65,30 +72,78 @@ public class Main {
                         System.out.println("程序已终止");
                         return;
                     } else if ("y".equals(value.toLowerCase())) {
-                        deleteOldProjectDir(file1.getPath());
-                        if (!file1.mkdir()) {
-                            System.out.println("目录创建失败");
-                        }
-                        String mavenPath = projectPath + "/" + proMavenDir;
-                        File file2 = new File(mavenPath);
-                        file2.mkdirs();
-
-                        String javaDir = mavenPath + "java";
-                        String resourcesDir = mavenPath + "resources";
-
-                        new File(javaDir).mkdir();
-                        new File(resourcesDir).mkdir();
-
-                        String newProDir = proDir.replaceAll("\\.", "/");
-                        String dir = javaDir + "/" + newProDir;
-
-                        for (String s : list) {
-                            new File(dir + "/" + s).mkdirs();
-                        }
-                    }
-                    if (!"y".equals(value.toLowerCase()) && !"n".equals(value.toLowerCase())) {
+                        isCreate = true;
+                    } else if (!"y".equals(value.toLowerCase()) && !"n".equals(value.toLowerCase())) {
                         System.out.println("输入无效程序退出");
+                        return;
                     }
+                }
+                if (isCreate) {
+                    deleteOldProjectDir(file1.getPath());
+                    System.out.println("正在创建: " + file1.getPath());
+                    if (!file1.mkdir()) {
+                        System.out.println("目录创建失败");
+                    }
+                    String mavenPath = projectPath + "/" + proMavenDir;
+                    File file22 = new File(mavenPath);
+                    System.out.println("正在创建: " + proMavenDir);
+                    file22.mkdirs();
+
+                    String javaDir = mavenPath + "java";
+                    String resourcesDir = mavenPath + "resources";
+                    System.out.println("正在创建: " + "java");
+                    new File(javaDir).mkdir();
+                    System.out.println("正在创建: " + "resources");
+                    new File(resourcesDir).mkdir();
+
+                    String newProDir = proDir.replaceAll("\\.", "/");
+                    String dir = javaDir + "/" + newProDir;
+                    System.out.println("正在创建: " + newProDir);
+                    for (String s : list) {
+                        System.out.println("正在创建: " + newProDir + "/" + s);
+                        new File(dir + "/" + s).mkdirs();
+                    }
+
+                    //开始copy 文件
+                    String projectPath1 = getProjectPath();
+
+                    File file2 = new File(projectPath1 + "/application.properties");
+                    if (!file2.exists() || !file2.isFile()) {
+                        System.out.println("Error: application.properties不存在");
+                    }
+                    File file3 = new File(projectPath + "/" + proMavenDir + "resources/application.properties");
+
+
+                    try {
+                        System.out.println("正在拷贝: application.properties ");
+                        copyFile(file2.getPath(), file3.getPath());
+
+                        System.out.println("正在拷贝: pom.xml  ");
+                        copyFile(projectPath1 + "/pom.xml", projectPath + "/pom.xml");
+
+                        System.out.println("正在拷贝: JooqDao.java");
+
+                        File dao = new File(projectPath + "/" + proMavenDir + "java/" + newProDir + "/dao");
+                        if (!dao.exists() || !dao.isDirectory()) {
+                            System.out.println("没找到dao目录，不执行copy JooqDao.java");
+                        } else {
+                            copyFile(projectPath1 + "/JooqDao.java", projectPath + "/" + proMavenDir + "java/" + newProDir + "/dao/JooqDao.java");
+                        }
+
+                        System.out.println("正在拷贝: README.md  ");
+                        copyFile(projectPath1 + "/README.md", projectPath + "/README.md");
+
+                        System.out.println("正在拷贝: .gitignore ");
+                        copyFile(projectPath1 + "/.gitignore", projectPath + "/.gitignore");
+
+                        System.out.println("正在拷贝: logback.xml  ");
+                        copyFile(projectPath1 + "/logback.xml", projectPath + "/" + proMavenDir + "resources/logback.xml");
+
+                        System.out.println("完成");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
 
                 }
 
@@ -103,6 +158,46 @@ public class Main {
 
     }
 
+
+    public static void copyFile(String srcFileName, String destFileName) throws Exception {
+        File srcFile = new File(srcFileName);
+
+
+        // 判断目标文件是否存在
+        File destFile = new File(destFileName);
+        // 复制文件
+        int byteread = 0; // 读取的字节数
+        InputStream in = null;
+        OutputStream out = null;
+
+        try {
+            in = new FileInputStream(srcFile);
+            out = new FileOutputStream(destFile);
+            byte[] buffer = new byte[1024];
+
+            while ((byteread = in.read(buffer)) != -1) {
+                out.write(buffer, 0, byteread);
+            }
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        } finally {
+            try {
+                if (out != null)
+                    out.close();
+                if (in != null)
+                    in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 删除文件及文件夹
+     *
+     * @param path
+     * @return
+     */
     public static String deleteOldProjectDir(String path) {
         File file = new File(path);
         if (file.exists()) {
@@ -121,4 +216,29 @@ public class Main {
         }
         return "SUCCESS";
     }
+
+
+    /**
+     * 获取项目所在路径(包括jar)
+     *
+     * @return
+     */
+    public static String getProjectPath() {
+
+        java.net.URL url = Main.class.getProtectionDomain().getCodeSource()
+                .getLocation();
+        String filePath = null;
+        try {
+            filePath = java.net.URLDecoder.decode(url.getPath(), "utf-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (filePath.endsWith(".jar"))
+            filePath = filePath.substring(0, filePath.lastIndexOf("/") + 1);
+        java.io.File file = new java.io.File(filePath);
+        filePath = file.getAbsolutePath();
+        return filePath;
+    }
+
+
 }
